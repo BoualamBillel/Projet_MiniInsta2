c10d16d2b1e57f0443cde6b7861ec4e8a7307aa8
Billel Boualam <boualambillelsofien@gmail.com>
2025-07-18 16:38:24 +0200

    added grid on galerie nav button

--- galerie.php
+++ galerie.php
@@ -18,17 +18,19 @@
     <main>
         <section class="bg-white lg:grid  flex justify-center pt-10 px-5 dark:bg-gray-900 h-auto items-center flex-col">
 
+        <div class="grid grid-cols-2 gap-3 text-center pb-5  ">
             <a 
-                class="inline-block rounded border border-white-600 bg-pink-600 px-5 py-3 mt-5 font-medium text-white shadow-sm transition-colors hover:bg-pink-500 flex justify-center items-center"
+                class="inline-block rounded border border-white-600 bg-pink-600 px-5 py-3  font-medium text-white shadow-sm transition-colors hover:bg-pink-500 flex justify-center items-center"
                 href="upload.php">
                 Upload ta Photo
             </a>
 
             <a 
-                class="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900 dark:border-white-700 dark:text-gray-200 dark:hover:bg-gray-800 dark:hover:text-white flex items-center justify-center m-5"
+                class="inline-block rounded border border-gray-200 px-5 py-3 font-medium text-gray-700 shadow-sm transition-colors hover:bg-gray-50 hover:text-gray-900 dark:border-white-700 dark:text-gray-200 dark:hover:bg-gray-800 dark:hover:text-white flex items-center justify-center "
                 href="index.php">
                 Retourne à l'acceuil
             </a>
+            </div>
             <div class=" text-lg text-center flex flex-col gap-3 pb-5 dark:text-white">
                 <h1 class="text-bold text-2xl">🎨 La Galerie</h1>
                 <p class="text-xl">Un mur d’inspiration, une toile vivante.</p>
